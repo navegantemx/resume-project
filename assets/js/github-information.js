31887cd7214c03959165adc6359af5d35e73cec9
Ubuntu <ubuntu@ip-172-31-45-207.ec2.internal>
2019-08-07 01:45:43 +0000

    Fix bugs for the gh-user-data & gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,4 @@
+    
 function userInformationHTML(user) {
     return `
         <h2>${user.name}
@@ -37,6 +38,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -70,3 +73,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
